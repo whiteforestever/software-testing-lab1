468ee6eefe36b57eb13c854ec4bb5426b9cf4d96
whiteforestever <danila.whiteman@gmail.com>
2023-03-04 13:27:49 +0300

    add parameterized sin

--- src/test/java/functionTest/FunctionSinUtilTest.java
+++ src/test/java/functionTest/FunctionSinUtilTest.java
@@ -5,6 +5,8 @@ import static java.lang.Float.NaN;
 
 import org.junit.Test;
 import function.FunctionSinUtil;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,39 +30,49 @@ public class FunctionSinUtilTest {
         assertEquals(NaN, actual, FunctionSinUtil.PRECISION);
     }
 
-    // для угла в пределах [-π, π]
-    @Test
-    public void testSinPrecision() {
-        HashMap<Double, Double> arrayTestValues = new HashMap<>();
+    // для 1 квадранта
+    @ParameterizedTest(name = "x={0}")
+    @ValueSource(doubles = {Math.PI / 6, Math.PI / 4, Math.PI / 3, Math.PI / 2})
+    public void testSinFirstQuadrant(double val) {
+        double expected = Math.sin(val);
+        double actual = FunctionSinUtil.sin(val);
+        assertEquals(expected, actual, FunctionSinUtil.PRECISION);
+    }
 
-        // 1 и 4 квадранты
-        arrayTestValues.put(Math.PI / 6, 0.5);
-        arrayTestValues.put(Math.PI / 4, Math.sqrt(2) / 2);
-        arrayTestValues.put(Math.PI / 3, Math.sqrt(3) / 2);
-        arrayTestValues.put(Math.PI / 2, 1d);
-        arrayTestValues.put(2*Math.PI / 3, Math.sqrt(3) / 2);
-        arrayTestValues.put(3* Math.PI / 4, Math.sqrt(2) / 2);
-        arrayTestValues.put(5*Math.PI / 6, 0.5);
+    // для 2 квадранта
+    @ParameterizedTest(name = "x={0}")
+    @ValueSource(doubles = {2*Math.PI / 3, 3* Math.PI / 4, 5*Math.PI / 6})
+    public void testSinSecondQuadrant(double val) {
+        double expected = Math.sin(val);
+        double actual = FunctionSinUtil.sin(val);
+        assertEquals(expected, actual, FunctionSinUtil.PRECISION);
+    }
 
-        // 2 и 3 квадранты
-        arrayTestValues.put(-Math.PI / 6, -0.5);
-        arrayTestValues.put(-Math.PI / 4, -Math.sqrt(2) / 2);
-        arrayTestValues.put(-Math.PI / 3, -Math.sqrt(3) / 2);
-        arrayTestValues.put(-Math.PI / 2, -1d);
-        arrayTestValues.put(-2*Math.PI / 3, -Math.sqrt(3) / 2);
-        arrayTestValues.put(-3* Math.PI / 4, -Math.sqrt(2) / 2);
-        arrayTestValues.put(-5*Math.PI / 6, -0.5);
+    // для 3 квадранта
+    @ParameterizedTest(name = "x={0}")
+    @ValueSource(doubles = {-Math.PI / 6, -Math.PI / 4, -Math.PI / 3, -Math.PI / 2})
+    public void testSinThirdQuadrant(double val) {
+        double expected = Math.sin(val);
+        double actual = FunctionSinUtil.sin(val);
+        assertEquals(expected, actual, FunctionSinUtil.PRECISION);
+    }
 
-        // граничные значения
-        arrayTestValues.put(-Math.PI, 0d);
-        arrayTestValues.put(Math.PI, 0d);
+    // для 4 квадранта
+    @ParameterizedTest(name = "x={0}")
+    @ValueSource(doubles = {-2*Math.PI / 3, -3* Math.PI / 4, -5*Math.PI / 6})
+    public void testSinFourthQuadrant(double val) {
+        double expected = Math.sin(val);
+        double actual = FunctionSinUtil.sin(val);
+        assertEquals(expected, actual, FunctionSinUtil.PRECISION);
+    }
 
-        double expected, actual;
-        for (Map.Entry<Double, Double> entry : arrayTestValues.entrySet()) {
-            expected = entry.getValue();
-            actual = FunctionSinUtil.sin(entry.getKey());
-            assertEquals(expected, actual, FunctionSinUtil.PRECISION);
-        }
+    // для граничных значений периода
+    @ParameterizedTest(name = "x={0}")
+    @ValueSource(doubles = {-Math.PI, Math.PI})
+    public void testSinBorders(double val) {
+        double expected = Math.sin(val);
+        double actual = FunctionSinUtil.sin(val);
+        assertEquals(expected, actual, FunctionSinUtil.PRECISION);
     }
 
 }
